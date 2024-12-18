e20fa8f7d038dd4be1cbe7a9e6da77d080222fb0
Helena <helastekerova7@gmail.com>
2024-12-18 15:40:42 +0100

    DAY 22: add images and space column

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ col1, col2 = st.columns(2)
 with col1:
     st.image("images/photo.JPG")
 try:
-    with open("images/photo.JPG", "rb") as f:
+    with open("images/photo.jpeg", "rb") as f:
         st.image(f.read())
 except FileNotFoundError:
     st.error("Soubor nebyl nalezen!")
